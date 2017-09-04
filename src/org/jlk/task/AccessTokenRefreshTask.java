91e0f86e0699c8de7b92492c1d53a09a021f6a13
jiangliangkun <jiangliangkun@allook.cn>
2017-09-04 10:22:50 +0800

    fix bugs

--- src/org/jlk/task/AccessTokenRefreshTask.java
+++ src/org/jlk/task/AccessTokenRefreshTask.java
@@ -14,11 +14,15 @@ public class AccessTokenRefreshTask extends TimerTask{
 	public void run() {
 		log.info("Refreshing AccessToken ...");
 		
-		AuthInfoSingleton authInfo = AuthInfoSingleton.getInstance();
-		String accessToken = Util.refreshtoken(authInfo.getOpenID(), authInfo.getRefreshToken()).getAccessToken();
-		authInfo.setAccessToken(accessToken);
-		
-		log.info("New AccessToken : [" + accessToken + "]");
+		try {
+			AuthInfoSingleton authInfo = AuthInfoSingleton.getInstance();
+			String accessToken = Util.refreshtoken(authInfo.getOpenID(), authInfo.getRefreshToken()).getAccessToken();
+			authInfo.setAccessToken(accessToken);
+			
+			log.info("New AccessToken : [" + accessToken + "]");
+		} catch (Exception e) {
+			log.error(e);
+		}
 	}
 
 }
